59c029d47443764539d19357e1a9d782b92099f7
dipta-sdd <sankarsandipta@gmail.com>
2025-07-30 16:22:18 +0600

    add vendor folder for autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-anchor-bay/campaign-bay',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd9988782358a21c6f0cfa74e131d6f9236a33487',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-anchor-bay/campaign-bay' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd9988782358a21c6f0cfa74e131d6f9236a33487',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
