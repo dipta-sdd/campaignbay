bbae040618906f0e534f5bf1d5521c54676e0756
dipta-sdd <sankarsandipta@gmail.com>
2025-09-28 14:39:04 +0600

    jj

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
-const defaults = require("@wordpress/scripts/config/webpack.config");
+// webpack.config.js
+
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-  ...defaults,
+  ...defaultConfig,
+
+  // This forces all dependencies (React, wp-components, etc.) to be
+  // included directly in your final admin.js and public.js files.
   externals: {},
+
   entry: {
     admin: "./src/admin/index.js",
     public: "./src/public/index.js",
   },
+
   output: {
-    ...defaults.output,
+    ...defaultConfig.output,
     filename: "[name].js",
   },
+
+  // This is the new part.
+  // It adds the Bundle Analyzer to the list of Webpack plugins.
+  // When you run `npm run build`, it will automatically open a browser
+  // tab showing a visual map of your bundled files.
+  plugins: [
+    ...defaultConfig.plugins,
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // Generates an HTML report file
+      openAnalyzer: true, // Automatically opens the report in your browser
+      reportFilename: "bundle-report.html", // The name of the report file
+    }),
+  ],
 };
