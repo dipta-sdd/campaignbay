1e2d81a9b0278dce62d87fbe0e6d0f516878d5ff
dipta-sdd <sankarsandipta@gmail.com>
2025-10-05 15:16:28 +0600

    dual build setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,54 @@
+const defaults = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isLegacy = process.env.BUILD_TARGET === "legacy";
 
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  mode: isProduction ? 'production' : 'development',
-  entry: {
-    admin: './src/admin/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
-    clean: true, // Cleans the build directory before each build
-  },
-  devtool: isProduction ? false : 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
+module.exports = isLegacy
+  ? {
+      entry: {
+        admin: "./src/admin/index.js",
       },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+      output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].js",
+        clean: true,
+      },
+      devtool: false,
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: "babel-loader",
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "postcss-loader",
+              "sass-loader",
+            ],
+          },
         ],
       },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
-  devServer: {
-    // Webpack dev server settings (optional, for `npm run start`)
-    devMiddleware: {
-      writeToDisk: true, // Writes files to the build folder
-    },
-    hot: false,
-    liveReload: true,
-    port: 9000,
-  },
-};
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: "[name].css",
+        }),
+      ],
+    }
+  : {
+      ...defaults,
+      externals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+      entry: {
+        admin: "./src/admin/index.js",
+      },
+      output: {
+        ...defaults.output,
+        filename: "[name].js",
+      },
+    };
