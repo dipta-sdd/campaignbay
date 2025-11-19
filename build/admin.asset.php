58a69fddeb82ec833d4a1d324e6c9ac22f93cf27
dipta-sdd <sankarsandipta@gmail.com>
2025-11-19 18:24:46 +0600

    removed build folder

--- build/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '73b26e3d260d03c31309');
