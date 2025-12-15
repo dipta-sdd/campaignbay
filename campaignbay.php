103358201fc5ab6ac6851aae6fbffa3e4b0b69d7
dipta-sdd <sankarsandipta@gmail.com>
2025-12-15 17:23:34 +0600

    release-1.0.6

--- campaignbay.php
+++ campaignbay.php
@@ -8,8 +8,8 @@
  * Requires PHP:      7.0
  * Requires Plugins:  woocommerce
  * WC requires at least: 6.1
- * Version:           1.0.5
- * Stable tag:        1.0.5
+ * Version:           1.0.6
+ * Stable tag:        1.0.6
  * Author:            WPAnchorBay
  * Author URI:        https://wpanchorbay.com
  * License:           GPLv2 or later
@@ -26,7 +26,7 @@ if (!defined('WPINC')) {
 define('CAMPAIGNBAY_PATH', plugin_dir_path(__FILE__));
 define('CAMPAIGNBAY_DIR', plugin_dir_path(__FILE__));
 define('CAMPAIGNBAY_URL', plugin_dir_url(__FILE__));
-define('CAMPAIGNBAY_VERSION', '1.0.5');
+define('CAMPAIGNBAY_VERSION', '1.0.6');
 define('CAMPAIGNBAY_PLUGIN_NAME', 'campaignbay');
 define('CAMPAIGNBAY_TEXT_DOMAIN', 'campaignbay');
 define('CAMPAIGNBAY_OPTION_NAME', 'campaignbay');
